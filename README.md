5268139f44b4c849508a184e07eea6fb10e5655a
h.u.g.u.rp <h.u.g.u.rp@pm.me>
2022-11-03 12:28:55 +0100

    initial wildcard test

---
+++ README.md
@@ -0,0 +1,3 @@
+Testing wildcard subdomains for radio4000 radio profiles by slug.
+
+- https://www.smashingmagazine.com/2021/11/nextjs-wildcard-subdomains/#client-side-with-useeffect
---
+++ pages/index.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home(props) {
+	const [wildcard, setWildcard] = useState('')
+	useEffect(() => {
+		setWildcard(window.location.hostname.split('.')[0])
+	}, [])
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Radio</title>
+				<meta name="description" content="Radio" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main className={styles.main}>
+				<p>props.wildcard {props.wildcard}</p>
+				<p>wildcard {wildcard}</p>
+			</main>
+
+			<footer className={styles.footer}>
+				radio4000 radio profile
+			</footer>
+		</div>
+	)
+}
+
+export async function getServerSideProps(context: any) {
+	let parts = context.req.headers.host.split('.')
+	if (
+		parts.length === 2
+		&& parts.indexOf(process.env.ROOT_DOMAIN) > -1
+	) {
+		const wildcard = parts.filter((part: string) => {
+			return part !== process.env.ROOT_DOMAIN
+		})[0]
+		return { props: { wildcard } }
+	} else {
+		return { props: { } }
+	}
+}
