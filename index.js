ee9b9a1534cf66cfa3102fcf3ce09f442301a773
h.u.g.u.rp <h.u.g.u.rp@pm.me>
2022-11-05 13:02:21 +0100

    0.0.29, <r4-radio components-url, single-channel, /s/slug/channel/>

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 export default class R4Radio extends HTMLElement {
 	static get observedAttributes() {
-		return ['hostname', 'slug']
+		return ['hostname', 'channel', 'components-url', 'single-channel']
+	}
+	/* the cdn url to load the components from */
+	get componentsUrl() {
+		return this.getAttribute('components-url')
+	}
+	/* the r4-app is instanciated to browse/play 1 channel only  */
+	get singleChannel() {
+		return this.getAttribute('single-channel') === 'true'
 	}
 	/* the hostname is the root domain,
 		 on which the r4 radio profile app is running */
@@ -8,7 +16,7 @@ export default class R4Radio extends HTMLElement {
 		return this.getAttribute('hostname')
 	}
 	/* the slug comes from the wildcard subdomain */
-	get slug() {
+	get channel() {
 		if (!window.location.hostname.endsWith('.' + this.hostname)) {
 			return ''
 		}
@@ -22,16 +30,26 @@ export default class R4Radio extends HTMLElement {
 	}
 
 	async connectedCallback() {
+		await this.importStyles()
 		await this.importComponents()
-		if (this.slug) {
-			this.renderChannel(this.slug)
+		if (this.channel) {
+			this.setAttribute('channel', this.channel)
+			this.renderChannel(this.channel)
 		} else {
 			this.renderHome()
 		}
 	}
+	async importStyles() {
+		/* `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@radio4000/components@0.0.29/styles/index.css" />` */
+		const $stylesLink = document.createElement('link')
+		$stylesLink.setAttribute('rel', 'stylesheet')
+		$stylesLink.setAttribute('href', `${this.componentsUrl}/styles/index.css`)
+		document.querySelector('head').append($stylesLink)
+		console.log('CSS styles imported and inserted', $stylesLink)
+	}
 	async importComponents() {
-		const Components = await import('https://cdn.jsdelivr.net/npm/@radio4000/components@0.0.28/dist/index.js')
-		console.log('Components imported', Components)
+		const Components = await import(`${this.componentsUrl}/dist/index.js`)
+		console.log('Javascript web-components imported', Components)
 	}
 
 	renderChannel(slug) {
@@ -39,6 +57,7 @@ export default class R4Radio extends HTMLElement {
 		const $app = document.createElement('r4-app')
 		$app.setAttribute('href', window.location.origin)
 		$app.setAttribute('channel', slug)
+		this.singleChannel && $app.setAttribute('single-channel', this.singleChannel)
 		this.append($app)
 	}
 	renderHome() {
